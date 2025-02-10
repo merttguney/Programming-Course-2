699fbd6760b1bee936e102a1e61de07c9a6dbd8a
Mert Güney <k.mertguney@gmail.com>
2025-02-10 23:47:10 +0300

    Added files via upload

---
+++ Programming 2 Course/GUI/src/Main.groovy
@@ -0,0 +1,7 @@
+import javax.swing.JButton
+import javax.swing.JLabel
+import javax.swing.JTextField
+
+static void main(String[] args) {
+    println "Hello world!"
+  } private JButton button1 ; private JButton button2 ; private JButton button3 ; private JButton button1 ; private JButton button2 ; private JButton button3 ; private JLabel Name ; private JTextField textField1 ; private JTextField textField2 ; private JTextField textField3 ; private JButton button1 ; private JButton button1 ; private JButton button1 ;
---
+++ Programming 2 Course/GUI_Labwork/src/main/java/com/mycompany/gui_labwork/GUI_Labwork.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui_labwork;
+
+/**
+ *
+ * @author mertguney
+ */
+public class GUI_Labwork {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
