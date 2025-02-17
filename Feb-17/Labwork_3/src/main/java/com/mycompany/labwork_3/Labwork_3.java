5e00c813b87f722673aadc40c9bfda3c08c39d62
Mert Güney <k.mertguney@gmail.com>
2025-02-17 22:44:51 +0300

    Initial commit or latest changes

---
+++ Feb-17/Labwork_3/src/main/java/com/mycompany/labwork_3/Labwork_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labwork_3;
+
+/**
+ *
+ * @author mertguney
+ */
+public class Labwork_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
