3cd1111de9bad20ed5e19cdd6e14ddfa7218982b
Mert Güney <k.mertguney@gmail.com>
2025-02-24 11:43:49 +0300

    Feb24 Lab completed(4th week).

---
+++ Labwork_4/src/main/java/Labwork_4.java
@@ -0,0 +1,32 @@
+public class Labwork_4 {
+
+    public static void main(String[] args) {
+        // Write Java code that creates the multiplication table of numbers 1 to 10.
+        
+        // Write the Java Code that creates the star pyramid(Nested For Loop)
+        
+//        for(int i = 1;i <= 5; i++)
+//        {
+//            for(int j = 1; j<=i; j++ ) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        // Check whether the number variable is greater than 0. If the number is greater than 0, "The number is positive is printed.
+        // Otherwise, "The number is negative or zero." Write the java code that prints.
+
+        int num = 10;
+
+        if(num > 0)
+        {
+            System.out.println("The number is positive.");
+        }
+        else
+        {
+            System.out.println("The number is negative.");
+        }
+
+
+    }
+}
